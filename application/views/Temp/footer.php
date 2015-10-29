1822897575837e1d3c4a7abf9d06dfccbb1ffec5
caitlynod <CaitlynODriscoll@Caitlyns-MacBook-Pro.local>
2015-10-29 16:45:10 -0400

    footer Nav

--- application/views/Temp/footer.php
+++ application/views/Temp/footer.php
@@ -1,3 +1,15 @@
+<ul class="small-block-grid-9 columns">
+	<li>1</li>
+	<li>2</li>
+	<li>3</li>
+	<li>4</li>
+	<li>5</li>
+	<li>6</li>
+	<li>7</li>
+	<li>8</li>
+	<li>9</li>
+	
+</ul>
 <script src="<?php echo base_url().'js/vendor/jquery.js';?>"></script>
 <script src="<?php echo base_url().'js/foundation.min.js';?>"></script>
 <script>
