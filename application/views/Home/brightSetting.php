0682d289f380dc239d9f080f7ad1cf1a5d9b9a6d
caitlynod <CaitlynODriscoll@Caitlyns-MacBook-Pro.local>
2015-10-29 19:34:05 -0400

    arrows added to brightness setting code

--- application/views/Home/brightSetting.php
+++ application/views/Home/brightSetting.php
@@ -1,4 +1,5 @@
-
-	<div class="small-4 columns">
-		
-	</div></section>
+<div class="small-4 columns">
+	<img src="<?php echo base_url().'images/upArrow.png';?>">
+	<img src="<?php echo base_url().'images/downArrow.png';?>">
+</div>
+</section>
