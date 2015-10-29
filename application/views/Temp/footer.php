a9603f4d14b55e136d8f4d720fb418902cc8e1aa
caitlynod <CaitlynODriscoll@Caitlyns-MacBook-Pro.local>
2015-10-29 16:32:17 -0400

    added retina.js

--- application/views/Temp/footer.php
+++ application/views/Temp/footer.php
@@ -5,5 +5,6 @@ $(document).foundation();
 </script>
 <script type="text/javascript" src="<?php echo base_url().'js/clock.js';?>"></script>
 <script type="text/javascript" src="<?php echo base_url().'js/main.js';?>"></script>
+<script type="text/javascript" src="<?php echo base_url().'js/retina.min.js';?>">></script>
 </body>
 </html>
